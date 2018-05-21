ae40343844fe0a2988d24fd80b279ab9aa6848e3
Mark Grama <mark@grama.ru>
2018-05-21 17:00:58 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,103 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+//import 'bootstrap/dist/css/bootstrap.css';
+import { Navbar, NavItem, Nav, Grid, Row, Col } from 'react-bootstrap';
+import 'bootswatch/journal/bootstrap.css'
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+const PLACES = [
+  { name: 'Palo Alto', zip: '94303' },
+  { name: 'San Jose', zip: '94088' },
+  { name: 'Santa Cruz', zip: '95062' },
+  { name: 'Honolulu', zip: '96803' }
+];
 
-export default App;
+class WeatherDisplay extends Component {
+  constructor() {
+    super();
+    this.state = {
+      weatherData: null,
+    };
+  }
+  componentDidMount() {
+    const zip = this.props.zip;
+    const URL = 'http://api.openweathermap.org/data/2.5/weather?q=' +
+      zip +
+      '&appid=b1b35bba8b434a28a0be2a3e1071ae5b&units=imperial';
+    fetch(URL).then(res => res.json()).then(json => {
+      this.setState({ weatherData: json });
+    });
+  }
+  render() {
+     const weatherData = this.state.weatherData;
+     if (!weatherData) return <div>Loading</div>;
+     const weather = weatherData.weather[0];
+     const iconUrl = 'http://openweathermap.org/img/w/' + weather.icon + '.png';
+     return (
+       <div>
+         <h1>
+           {weather.main} in {weatherData.name}
+           <img src={iconUrl} alt={weatherData.description} />
+         </h1>
+         <p>Current: {weatherData.main.temp}°</p>
+         <p>High: {weatherData.main.temp_max}°</p>
+         <p>Low: {weatherData.main.temp_min}°</p>
+         <p>Wind Speed: {weatherData.wind.speed} mi/hr</p>
+       </div>
+     );
+   }
+ }
+
+class App extends Component {
+  constructor() {
+      super();
+      this.state = {
+        activePlace: 0
+      };
+    }
+    render() {
+      const activePlace = this.state.activePlace;
+      return (
+        <div>
+          <Navbar>
+            <Navbar.Header>
+              <Navbar.Brand>
+                React simple Weather App
+              </Navbar.Brand>
+            </Navbar.Header>
+          </Navbar>
+          <Grid>
+            <Row>
+              <Col md={4} sm={4}>
+                <h3>Select a city</h3>
+                <Nav
+                  bsStyle='pills'
+                  stacked
+                  activeKey={activePlace}
+                  onSelect={index => {
+                    this.setState({ activePlace: index });
+                  }}
+                  >
+                    {Places}
+                  </Nav>
+              </Col>
+            </Row>
+          </Grid>
+        </div>
+        <div className='App'>
+          {PLACES.map((place, index) => (
+            <button
+              key={index}
+              onClick={() => {
+                this.setState({ activePlace: index });
+              }}
+            >
+              {place.name}
+            </button>
+          ))}
+          <WeatherDisplay key={activePlace} zip={PLACES[activePlace].zip} />
+        </div>
+      );
+    }
+  }
+
+  export default App;
